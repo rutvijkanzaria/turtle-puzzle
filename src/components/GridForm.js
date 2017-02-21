f6fc59da91305f338c52d2518543311894c51b85
Rutvij Kanzaria <rutvijkanzaria@gmail.com>
2017-02-22 02:08:28 +0530

    Grid generation completed

--- src/components/GridForm.js
+++ src/components/GridForm.js
@@ -3,13 +3,24 @@
 import React, {Component} from 'react';
 
 class GridForm extends Component {
+  handleSubmit = e =>{
+    e.preventDefault();
+    this.props.createGrid(this.size.value);
+  }
+
 	render(){
 		return(
-			<form>
+			<form onSubmit={this.handleSubmit}>
         <div className="form-group">
           <label htmlFor="gridinput">Enter grid size</label>
           <div className="input-group">
-            <input type="number" className="form-control" id="gridinput" placeholder="Enter grid size..." />
+            <input 
+              type="number" 
+              ref={(input) =>this.size = input}
+              className="form-control" 
+              id="gridinput" 
+              placeholder="Enter grid size..." 
+            />
             <span className="input-group-btn">
               <button type="submit" className="btn btn-primary">GENERATE</button>
             </span>
--- src/containers/Turtle.js
+++ src/containers/Turtle.js
@@ -122,21 +122,65 @@ const INITIAL_STATE = [
       "obstacle": false
     }
   ]
-]
+];
 
 class Turtle extends Component {
   constructor(props){
     super(props);
     this.state = {
-      gridArray: INITIAL_STATE
+      gridSize: 5,
+      gridArray: []
     }
   }
+
+  componentDidMount() {
+    this.constructGrid(this.state.gridSize);
+  }
+
+  createGrid = value => {
+    this.setState({gridSize: value});
+    this.constructGrid(value);
+  }
+
+  constructGrid = (size) => {
+    let grid = [];
+    for(let i = 0; i < size; i++){
+      let row = [];
+      for(let j = 0; j < size; j++){
+        let block = {};
+        if(i === 0 && j === 0){
+          block = {
+            turtle: ["true", "north"],
+            obstacle: false
+          }
+        } else {
+          block = {
+            turtle: ["false", "north"],
+            obstacle: false
+          }
+        }
+        row.push(block);
+      }
+      grid.push(row);
+    }
+    let obst = 0;
+    while(obst < size){
+      let i = Math.floor((Math.random() * size) + 0);
+      let j = Math.floor((Math.random() * size) + 0);
+      if(i !== 0 && j !==0 && !grid[i][j].obstacle){
+        grid[i][j].obstacle = true;
+        obst++;
+      }
+    }
+    this.setState({gridArray: grid});
+  }
+
   render() {
     return (
       <div className="container py-4">
         <div className="row">
           <div className="col-md-5">
-            <GridForm />
+            <GridForm createGrid={this.createGrid} />
             <MovementForm />
             <Movements />
           </div>
