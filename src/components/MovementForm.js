3b70bbfb1630b38a17807540cebf3d66513bfd23
Rutvij Kanzaria <rutvijkanzaria@gmail.com>
2017-02-23 02:20:59 +0530

    Added movements with setState inside a loop

--- src/components/MovementForm.js
+++ src/components/MovementForm.js
@@ -3,17 +3,81 @@
 import React, {Component} from 'react';
 
 class MovementForm extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      formInput: '',
+      movements: [],
+      showError: false
+    };
+  }
+
+  handleChange = e => {
+    let str = e.target.value;
+    let movements = this.state.movements.slice();
+    switch(str[str.length -1]){
+      case 'F':
+      case 'f':
+      case 'R':
+      case 'r':
+      case 'L':
+      case 'l':
+        movements.push(str[str.length - 1].toLowerCase());
+        this.setState({
+          formInput: str,
+          movements: movements,
+          showError: false
+        });
+        break;
+
+      case undefined: this.setState({
+          formInput: '',
+          movements: [],
+          showError: false
+        });
+        break;
+
+      default: this.setState({showError: true});
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.setMovements(this.state.movements);
+    this.setState({
+      formInput: '',
+      movements: [],
+      showError: false,
+    });
+    // console.log(this.movements.value);
+  }
+
 	render(){
 		return(
-			<form className="mt-5">
+			<form className="mt-5" onSubmit={this.handleSubmit}>
         <div className="form-group">
           <label htmlFor="movementinput">movement instructions</label>
           <div className="input-group">
-            <input type="text" id="movementinput" className="form-control" placeholder="Enter movements..." />
+            <input 
+              value={this.state.formInput}
+              onChange={this.handleChange}
+              type="text" 
+              id="movementinput" 
+              className="form-control" 
+              placeholder="Enter movements..." 
+            />
             <span className="input-group-btn">
               <button type="submit" className="btn btn-primary">MOVE TURTLE</button>
             </span>
           </div>
+          {
+            (this.state.showError) 
+            ?
+            <small className="form-text text-muted error">Please enter valid movement</small>
+            :
+            ''
+          }
         </div>
       </form>
 		);
--- src/containers/Turtle.js
+++ src/containers/Turtle.js
@@ -17,14 +17,16 @@ class Turtle extends Component {
     this.state = {
       gridSize: 5,
       gridArray: [],
-      currentPosition: [0, 0]
+      XPosition: 0,
+      YPosition: 0,
+      movements: []
     }
   }
 
   componentDidMount() {
     this.constructGrid(this.state.gridSize);
   }
-
+  
   createGrid = value => {
     this.setState({gridSize: value});
     this.constructGrid(value);
@@ -63,13 +65,149 @@ class Turtle extends Component {
     this.setState({gridArray: grid});
   }
 
+  setMovements = movements => {
+    console.log('movements', movements);
+    movements.forEach((move, index) => {
+      switch(move){
+        case 'r': 
+        case 'l': 
+          setTimeout(() => {
+          this.turnTurtle(move);
+        }, 0);
+          break;
+
+        default: 
+          setTimeout(() => {
+            this.moveTurtle();
+          }, 0);
+
+      }
+    });
+  }
+
+  turnTurtle = move => {
+    let grid = this.state.gridArray;
+    let direction = grid[this.state.XPosition][this.state.YPosition].turtle[1];
+    switch(direction){
+      case 'north': 
+        (move === 'r') 
+        ?
+          grid[this.state.XPosition][this.state.YPosition].turtle[1] = 'east'
+        :
+          grid[this.state.XPosition][this.state.YPosition].turtle[1] = 'west';
+        this.setState({
+          gridArray: grid
+        })
+
+        break;
+
+      case 'east': 
+        (move === 'r') 
+        ?
+          grid[this.state.XPosition][this.state.YPosition].turtle[1] = 'south'
+        :
+          grid[this.state.XPosition][this.state.YPosition].turtle[1] = 'north';
+        this.setState({
+          gridArray: grid
+        })
+        break;
+
+      case 'south':
+        (move === 'r') 
+        ?
+          grid[this.state.XPosition][this.state.YPosition].turtle[1] = 'west'
+        :
+          grid[this.state.XPosition][this.state.YPosition].turtle[1] = 'east';
+        this.setState({
+          gridArray: grid
+        })
+        break;
+
+      default: 
+        (move === 'r') 
+        ?
+          grid[this.state.XPosition][this.state.YPosition].turtle[1] = 'north'
+        :
+          grid[this.state.XPosition][this.state.YPosition].turtle[1] = 'south';
+        this.setState({
+          gridArray: grid
+        })
+
+    }
+  }
+
+  moveTurtle(){
+    let grid = this.state.gridArray;
+    let row = this.state.XPosition;
+    let col = this.state.YPosition;
+    let direction = grid[row][col].turtle[1];
+    switch(direction){
+      case 'north':
+        if(row != (this.state.gridSize - 1) && !grid[row+1][col].obstacle){
+          grid[row][col].turtle = "false";
+          row++;
+          grid[row][col].turtle = ["true", "north"];
+        }
+
+        this.setState({
+          gridArray: grid,
+          XPosition: row,
+          YPosition: col
+        });
+
+        break;
+
+      case 'east':
+        if(col != (this.state.gridSize - 1) && !grid[row][col + 1].obstacle){
+          grid[row][col].turtle[0] = "false";
+          col++;
+          grid[row][col].turtle = ["true", "east"];
+        }
+
+        this.setState({
+          gridArray: grid,
+          XPosition: row,
+          YPosition: col
+        });
+        break;
+
+      case 'south':
+        if(row != 0 && !grid[row-1][col].obstacle){
+          grid[row][col].turtle[0] = "false";
+          row--;
+          grid[row][col].turtle = ["true", "south"];
+        }
+
+        this.setState({
+          gridArray: grid,
+          XPosition: row,
+          YPosition: col
+        });
+        break;
+
+      default :
+        if(col != 0 && !grid[row][col-1].obstacle){
+          grid[row][col].turtle[0] = "false";
+          col--;
+          grid[row][col].turtle = ["true", "west"];
+        }
+
+        this.setState({
+          gridArray: grid,
+          XPosition: row,
+          YPosition: col
+        });
+
+    }
+  }
+
   render() {
     return (
       <div className="container py-4">
         <div className="row">
           <div className="col-md-5">
             <GridForm createGrid={this.createGrid} />
-            <MovementForm />
+            <MovementForm setMovements={this.setMovements} />
             <Movements />
           </div>
           <div className="col-md-7">
